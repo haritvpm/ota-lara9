9c8f13179eb317d6c10dff3f49cc0ebb98a49a33
haritvpm <harirs@gmail.com>
2019-03-04 18:03:23 +0530

    cdn links

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,6 +10,9 @@
 <meta http-equiv="Content-type"
       content="text/html; charset=utf-8">
 
+<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://cdnjs.cloudflare.com ">
+
+
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <!-- Font Awesome -->
