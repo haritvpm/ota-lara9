d2cbd223f4efb50e9f147a61051d077ffcf1b552
hari <harirs@gmail.com>
2024-02-20 09:05:02 +0530

    css from localhost

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -18,20 +18,21 @@
 
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" />
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/select2.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/adminltev3.css') }}" rel="stylesheet" />
-<link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet" />
-<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet" />
-<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet" />
-<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
-<link href="https://cdn.datatables.net/select/1.3.0/css/select.dataTables.min.css" rel="stylesheet" />
-<link href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css" rel="stylesheet" />
+<link href="{{ asset('css/all.css') }}" rel="stylesheet" />
+<!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet" /> -->
+<link href="{{ asset('css/jquery.dataTables.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/select.dataTables.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/buttons.dataTables.min.css') }}" rel="stylesheet" />
 <link href="{{ URL::asset('css/jquery-ui.min.css') }}" rel="stylesheet">
-<link rel="stylesheet" href="https://unpkg.com/vue-multiselect@2.1.6/dist/vue-multiselect.min.css">
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('css/vue-multiselect.min.css') }}">
+<!-- <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css"> -->
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+<link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
 
 <link href="{{ URL::asset('css/custom.css') }}" rel="stylesheet"><!-- for data table  -->
 
