2ba19e9e671473e7ebe8ff5ae21166347918539d
hari <harirs@gmail.com>
2024-03-27 07:48:42 +0530

    to lara10

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
+Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.', 'middleware' => ['auth:sanctum']], function () {
 
         Route::resource('designations', 'DesignationsController', ['except' => ['create', 'edit']]);
 
